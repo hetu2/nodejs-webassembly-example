db47c50194b6ba30635e2f616f07af8abb753015
hetu2 <hetu@saunalahti.fi>
2021-12-29 17:53:44 +0200

    comments removed

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// Assume add.wasm file exists that contains a single function adding 2 provided arguments
 const fs = require("fs");
 const fibJs = require("./fibonacci");
 const wasmBuffer = fs.readFileSync("./build/optimized.wasm");
@@ -6,11 +5,6 @@ const wasmBuffer = fs.readFileSync("./build/optimized.wasm");
 const N = 1000000000;
 
 WebAssembly.instantiate(wasmBuffer).then((wasmModule) => {
-  // Exported function live under instance.exports
-  // const add = wasmModule.instance.exports.add;
-  // const sum = add(5, 6);
-  // console.log(sum); // Outputs: 11
-
   console.log("\n");
   console.log("\n");
 
